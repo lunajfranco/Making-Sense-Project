3b23dd2fe866b8487a1477e4c1cff991b2a67c8f
Franco Luna <lunajfranco@outlook.com>
2021-09-15 21:00:19 -0300

    feat[nuevo CRUD] se agrego nueva clase, CRUD, Json y Controller

---
+++ Making Sense Project API/Model/Class/Rentals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Making_Sense_Project_API.Model.Class
+{
+    public class Rentals
+    {
+        public int IdRentals { get; set; }
+        public int IdCar { get; set; }
+        public int DniCustomer { get; set; }
+        public string DurationRental { get; set; }
+        public DateTime StartRentals { get; set; }
+        public DateTime EndRentals { get; set; }
+    }
+}
